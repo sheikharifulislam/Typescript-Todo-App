a357d68cc7ca8b28968ed9a7e22bdcdc14058bff
sheikh ariful <sheikharifulislam770@gmail.com>
2022-05-10 01:02:36 +0600

    comeple todo input functionally

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,23 @@ import { Todo } from "./model.js";
 
 const App: React.FC = () => {
     const [todo, setTodo] = useState<string>("");
-    const [todos, setTodtos] = useState<Todo[]>([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (todo) {
+            setTodos([
+                ...todos,
+                { _id: Date.now(), todo: todo, isDone: false },
+            ]);
+            setTodo("");
+        }
+    };
 
     return (
         <div className="App">
             <h1 className="heading">Taskify</h1>
-            <InputField todo={todo} setTodo={setTodo} />
+            <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         </div>
     );
 };
