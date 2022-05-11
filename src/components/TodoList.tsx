49c095f16b60853335716ec81cd5aa41fc25208e
sheikh ariful <sheikharifulislam770@gmail.com>
2022-05-12 00:03:41 +0600

    add react-beautiful-dnd

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,15 +9,29 @@ interface Props {
 
 export const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
     return (
-        <div className="todos">
-            {todos.map((todo) => (
-                <SingleTodo
-                    key={todo._id}
-                    todo={todo}
-                    todos={todos}
-                    setTodos={setTodos}
-                />
-            ))}
+        <div className="container">
+            <div className="todos">
+                <span className="todos_heading">Active Tasks</span>
+                {todos.map((todo) => (
+                    <SingleTodo
+                        key={todo._id}
+                        todo={todo}
+                        todos={todos}
+                        setTodos={setTodos}
+                    />
+                ))}
+            </div>
+            <div className="todos remove">
+                <span className="todos_heading">Completed Tasks</span>
+                {todos.map((todo) => (
+                    <SingleTodo
+                        key={todo._id}
+                        todo={todo}
+                        todos={todos}
+                        setTodos={setTodos}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
